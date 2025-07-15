c6b00ff6941fcad51ca4892d17320e019fe4314f
pedrrodev <pedrro.dev@gmail.com>
2025-07-15 13:45:39 -0300

    Add files via upload

---
+++ aulaJava/src/aulas/aula01.java
@@ -0,0 +1,26 @@
+package aulas;
+
+public class aula01 {
+    public static void main(String[] args) {
+        // Variáveis
+        double preco = 19.99;
+        boolean isJavaFun = true;
+        String saudacao = "Olá, programador";
+        char primeiraLetra = 'J';
+        int idade = 30;
+        String nome = "Paulo";
+
+        System.out.println("Preço: " + preco);
+        System.out.println("Java é divertido? " + isJavaFun);
+        System.out.println("Saudação: " + saudacao);
+        System.out.println("Primeira letra: " + primeiraLetra);
+        System.out.println("Meu nome é " + nome + " e tenho " + idade + " anos!");
+
+        idade = 31;
+
+        System.out.println("Nova idade: " + idade);
+
+
+    }
+
+}
